8381f07ebeed42cd6a12bced36e6e0a2dc859f82
tvanlong <longvan1173@gmail.com>
2023-09-07 21:33:19 +0700

    Khởi tạo

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className='flex items-center justify-center h-screen bg-gradient-to-b from-blue-300 via-blue-300 to-[#e7f1fa]'>
+      <div className='h-3/4 w-2/5 bg-[#f5f5f5] rounded-3xl'></div>
+    </div>
+  )
+}
+
+export default App
