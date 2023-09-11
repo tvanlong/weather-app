c7a3d136116a39362aa904d3be30f53ed334de4c
tvanlong <longvan1173@gmail.com>
2023-09-11 22:15:21 +0700

    get API bằng Axios, render UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,14 @@
-import { useState } from 'react'
-import { getCity } from './apis/city.api'
-import { useQuery } from '@tanstack/react-query'
+import { useEffect, useState } from 'react'
+// import { getCity } from './apis/city.api'
+// import { useQuery } from '@tanstack/react-query'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import hot from './assets/images/hot.png'
 import cold from './assets/images/cold.png'
+import { faEye } from '@fortawesome/free-regular-svg-icons'
+import { faWind } from '@fortawesome/free-solid-svg-icons'
+import { faCloudSun } from '@fortawesome/free-solid-svg-icons/faCloudSun'
+import axios from 'axios'
+import { WeatherOfCity } from './types/weather.type'
 
 const IMAGES = {
   bgHot: hot,
@@ -12,10 +18,47 @@ const IMAGES = {
 function App() {
   const [backgroundImage, setBackgroundImage] = useState<string>(IMAGES.bgHot)
   const [cityName, setCityName] = useState<string>('hanoi')
-  const { data } = useQuery({
-    queryKey: ['weather', cityName],
-    queryFn: () => getCity(cityName)
+  const [data, setData] = useState<WeatherOfCity>({
+    coord: { lon: 0, lat: 0 },
+    weather: [{ id: 0, main: '', description: '', icon: '' }],
+    base: '',
+    main: {
+      temp: 0,
+      feels_like: 0,
+      temp_min: 0,
+      temp_max: 0,
+      pressure: 0,
+      humidity: 0,
+      sea_level: 0,
+      grnd_level: 0
+    },
+    visibility: 0,
+    wind: { speed: 0, deg: 0, gust: 0 },
+    rain: { '1h': 0 },
+    clouds: { all: 0 },
+    dt: 0,
+    sys: { type: 0, id: 0, country: '', sunrise: 0, sunset: 0 },
+    timezone: 0,
+    id: 0,
+    name: '',
+    cod: 0
   })
+  // const { data: cityWeatherData } = useQuery({
+  //   queryKey: ['weather', cityName],
+  //   queryFn: () => getCity(cityName)
+  // })
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=32453de2fa72fdb0da4feeda287b7d6e`)
+      .then((res) => {
+        const cityNameResponse = (res.data.name as string).toLowerCase()
+        if (cityNameResponse !== cityName.toLowerCase()) {
+          setCityName(cityNameResponse)
+        }
+        setData(res.data)
+      })
+  }, [cityName])
 
   return (
     <div
@@ -51,16 +94,55 @@ function App() {
           <input
             type='text'
             placeholder='Search'
-            className='w-full h-14 py-3 px-12 bg-transparent text-xl text-white placeholder-white font-bold focus:shadow-2xl transition-shadow outline-none rounded-3xl border-white border-4'
+            value={cityName}
+            onChange={(e) => setCityName(e.target.value)}
+            className='w-full h-14 py-3 px-12 bg-[#ffffff2a] placeholder-shown:bg-transparent text-xl text-white placeholder-white font-bold focus:shadow-2xl transition-shadow outline-none rounded-3xl border-white border-4'
           />
+          <button className='absolute h-full right-4'>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              fill='none'
+              viewBox='0 0 24 24'
+              strokeWidth={3}
+              stroke='currentColor'
+              className='w-6 h-6 text-white'
+            >
+              <path
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                d='M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+              />
+            </svg>
+          </button>
         </div>
-        <div className='text-white text-5xl font-bold text-center drop-shadow-2xl text-shadow-3xl'>Hanoi , VN</div>
-        <div className='text-white text-xl font-medium text-center drop-shadow-2xl mt-2 mb-5'>9/9/2023, 9:34:14 PM</div>
+        <div className='text-white text-5xl font-bold text-center drop-shadow-2xl text-shadow-3xl'>
+          {data.name} , {data.sys.country}
+        </div>
+        <div className='text-white text-xl font-medium text-center drop-shadow-2xl mt-2 mb-5'>
+          {new Date().toLocaleString()}
+        </div>
         <div className='flex justify-center w-3/5 text-7xl text-white font-bold bg-[#fff6] rounded-3xl border-r-4 border-b-4 border-black px-5 py-10 m-auto'>
           <span className='text-shadow-4xl'>
-            28
+            {Math.round(data.main.temp - 273.15)}
             <sup className=''> o</sup>C
           </span>
+        </div>
+        <div className='text-white text-5xl font-bold text-center drop-shadow-2xl text-shadow-3xl mt-8'>
+          {data.weather[0].main}
+        </div>
+        <div className='flex justify-around text-white mt-10'>
+          <div className='flex flex-col items-center'>
+            <FontAwesomeIcon className='text-lg' icon={faEye} />
+            <span className='font-semibold tracking-wider mt-3'>{data.visibility} (m)</span>
+          </div>
+          <div className='flex flex-col items-center'>
+            <FontAwesomeIcon className='text-lg' icon={faWind} />
+            <span className='font-semibold tracking-wider mt-3'>{data.wind.speed} (m/s)</span>
+          </div>
+          <div className='flex flex-col items-center'>
+            <FontAwesomeIcon className='text-lg' icon={faCloudSun} />
+            <span className='font-semibold tracking-wider mt-3'>100 (%)</span>
+          </div>
         </div>
       </div>
     </div>
--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
-
-const queryClient = new QueryClient({
-  defaultOptions: {
-    queries: {
-      refetchOnWindowFocus: false
-    }
-  }
-})
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+const queryClient = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
 )
